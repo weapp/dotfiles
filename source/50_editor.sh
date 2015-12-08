dd25d6b6d7b24febea9cfaff5543f99f5c4fddc4
Manuel Albarrán <weap88@gmail.com>
2015-12-08 12:46:29 +0100

    migrating to rcm

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -10,9 +10,9 @@ fi
 export VISUAL="$EDITOR"
 
 
-alias openbash='$EDITOR ~/.dotfiles/source/'
-alias opendot='$EDITOR ~/.dotfiles/'
-alias openup='$EDITOR ~/.dotfiles/bin/up'
-alias cddot='cd ~/.dotfiles/'
+alias openbash='$EDITOR $DOTFILES/source/'
+alias opendot='$EDITOR $DOTFILES/'
+alias openup='$EDITOR $DOTFILES/bin/up'
+alias cddot='cd $DOTFILES/'
 alias openhost='$EDITOR /etc/hosts'
 alias reloadbash='source ~/.bash_profile'
