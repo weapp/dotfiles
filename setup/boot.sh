964bd4e9e54855e15871b477d0a2bf6b59ec21a0
Manuel Albarrán <weap88@gmail.com>
2015-12-08 15:53:06 +0100

    Added Brewfile to setup

--- setup/boot.sh
+++ setup/boot.sh
@@ -1,32 +1,56 @@
 #!/bin/sh
+
+set -o xtrace
+
 export DOTFILES=$HOME/dotfiles
 
 # installing brew
-which brew || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo xcodebuild -license accept
+if ! ( which brew > /dev/null ); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  sudo xcodebuild -license accept
+fi
+
 brew doctor
 brew update
 
-brew tap "thoughtbot/formulae"
-brew install "caskroom/cask/brew-cask"
-brew cask install "dropbox"
-brew install "rcm"
-brew install "git"
-brew install "hub"
-brew install "wget"
-brew install "curl"
-brew install "openssl"
-brew cask install "atom"
-brew cask install "telegram"
+function missing_brew_package () {
+  for i in "$@"; do
+    [ -z "$(brew ls --versions $i)" ] && return 0
+  done
+}
 
-if [ -d /Users/manu/.ssh ]
+if missing_brew_package rcm; then
+  brew tap "thoughtbot/formulae"
+  brew install "rcm"
+fi
+
+if missing_brew_package dropbox atom telegram; then
+  brew install "caskroom/cask/brew-cask"
+  brew cask install "dropbox"
+  brew cask install "atom"
+  brew cask install "telegram"
+fi
+
+if missing_brew_package git hub wget curl openssl; then
+  brew install "git"
+  brew install "hub"
+  brew install "wget"
+  brew install "curl"
+  brew install "openssl"
+fi
+
+if [ ! -d /Users/manu/.ssh ]; then
   mkdir -p $HOME/.ssh
   cp $HOME/Dropbox/ssh/* $HOME/.ssh
   chmod 600 $HOME/.ssh/id_rsa
 fi
 
-if [ -d $DOTFILES ]
+if [ ! -d $DOTFILES ]; then
   git clone git@github.com:weapp/dotfiles.git $DOTFILES
 fi
-lsrc -d ~/dotfiles/rc
+
+# lsrc -d ~/dotfiles/rc
 rcup -v -d ~/dotfiles/rc
+rcup -v # linking Dropbox rcs
+
+brew bundle
