4e331a768b11d76b8d1c73882a994831f9ef9fba
Manuel Albarrán <weap88@gmail.com>
2016-05-18 02:05:33 +0200

    Added k

--- rc/bash/source/50_misc.sh
+++ rc/bash/source/50_misc.sh
@@ -82,3 +82,5 @@ export GOPATH=$HOME/golang
 export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
+
+k () { cd ~/.k; eval "$(ruby k.rb "$@")"; }
---
+++ rc/k/k.rb
@@ -0,0 +1,49 @@
+require "shellwords"
+require "pp"
+require "yaml"
+require 'erb'
+require 'ostruct'
+
+def erb(template, vars)
+  ERB.new(template).result(OpenStruct.new(vars).instance_eval { binding })
+end
+
+def fetch(command_path, commands, vars={})
+  command_name = command_path.shift || commands["default"]
+  return ":" unless command_name
+  command = commands[command_name]
+  if !command && commands.keys
+    command = commands.keys.detect { |k| k.is_a? Symbol }
+    return ":" unless command
+    vars[command] = command_name
+    command = commands[command]
+  end
+  return fetch(command_path, command, vars) if command.is_a? Hash
+  erb(command, vars)
+end
+
+service_name, *command_path = *ARGV
+
+global = YAML.load(File.read("global.yml")) rescue {}
+service = YAML.load(File.read("#{service_name}.yml"))
+if service["alias"]
+  service_name = service["alias"]
+  service = YAML.load(File.read("#{service_name}.yml"))
+end
+
+base = File.expand_path(global.fetch("directory", "."), "~")
+directory = File.expand_path(service.fetch("directory", "."), base).shellescape
+
+env = global.fetch("env", {}).merge(service.fetch("env", {}))
+  .map { |key, value| "#{key}=#{value.shellescape} "}.join
+
+commands = global.fetch("commands", {}).merge(service.fetch("commands", {}))
+command = fetch(command_path, commands)
+
+# puts "\e[31m[ - #{service_name} - ]\e[0m"
+# print "\033]2;#{service_name}\007"
+
+red_name = "echo $'\\e[31m[ #{service_name.shellescape} | #{directory} $ #{command} ]\\e[0m'"
+title_name = "echo -en '\\033]2;#{service_name.shellescape}\\007'"
+
+puts "#{red_name};#{title_name};cd #{directory};#{env}#{command}"
