2e5f3b6cfd9ffd8d9cd9a4f4fd702e3137ff850a
Manuel <manuel.albarran@cabify.com>
2017-04-11 17:21:33 +0200

    an update

--- rb/app.rb
+++ rb/app.rb
@@ -1,17 +1,28 @@
 #!/usr/bin/env ruby
 
-require "logger"
-require "bundler/setup"
+
+%w(logger json ostruct bundler/setup).each(&method(:require))
 
 Bundler.require(:default, ENV["APP_ENV"] || "development")
-Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
+$LOAD_PATH.unshift("#{__dir__}/lib")
+Dir[File.dirname(__FILE__) + "/lib/**/*.rb"].sort.each { |file| require file }
+Dotenv.load
 
 class App
   def self.logger
     @logger ||= Logger.new(STDOUT)
   end
 
+  def self.debug(obj)
+    Pry::ColorPrinter.pp(obj)
+    obj
+  end
+
   def self.run
+  end
+
+  def self.env
+    ENV["APP_ENV"] || "development"
   end
 end
 
