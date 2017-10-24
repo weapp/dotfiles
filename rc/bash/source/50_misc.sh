15fb35cbf183b7658462f948c03e8932aa56d424
Manuel Albarran <manuel.albarran@cabify.com>
2017-10-24 15:28:57 +0200

    added newtab

--- rc/bash/source/50_misc.sh
+++ rc/bash/source/50_misc.sh
@@ -117,3 +117,4 @@ export PATH=$PATH:$GOROOT/bin
 
 k () { cd ~/.k; eval "$(ruby k.rb "$@")"; }
 
+function newtab { osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e "tell application \"Terminal\" to do script \"$*\" in selected tab of the front window"; }
